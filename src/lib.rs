fcdcbca15f57ac8f04ce1d06449468d4420769f0
Nikita Baksalyar <nikita.baksalyar@gmail.com>
2019-03-07 18:34:46 +0000

    Move k/v storage into its own module

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,41 @@
 #![allow(unsafe_code)]
 
 extern crate libc;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub mod kv_storage;
+
+pub struct World(pub *mut librdf_world);
+
+impl Drop for World {
+    fn drop(&mut self) {
+        unsafe {
+            librdf_free_world(self.0);
+        }
+    }
+}
+
+pub struct Model(pub *mut librdf_model);
+
+impl Drop for Model {
+    fn drop(&mut self) {
+        unsafe {
+            librdf_free_model(self.0);
+        }
+    }
+}
+
+pub struct Serializer(pub *mut librdf_serializer);
+
+impl Drop for Serializer {
+    fn drop(&mut self) {
+        unsafe {
+            librdf_free_serializer(self.0);
+        }
+    }
+}
+
